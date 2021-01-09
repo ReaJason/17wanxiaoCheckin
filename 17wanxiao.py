8939eea8fa667d89564dbec7e4e6aa4e50007750
lair20 <61303419+lair20@users.noreply.github.com>
2021-01-09 13:47:37 +0800

    Update 17wanxiao.py

--- 17wanxiao.py
+++ 17wanxiao.py
@@ -254,7 +254,7 @@ def check_in(username, password):
         #第一类健康打卡
         print(post_dict)
 
-        修改温度等参数
+        #修改温度等参数
         for j in post_dict['updatainfo']:  # 这里获取打卡json字段的打卡信息，微信推送的json字段
             if j['propertyname'] == 'temperature':  # 找到propertyname为temperature的字段
                 j['value'] = '36.2'  # 由于原先为null，这里直接设置36.2（根据自己学校打卡选项来）
